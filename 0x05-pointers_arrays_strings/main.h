2fdab8d0c890b1419ba24da95914a8336c94b817
Thoorby <ojothrby@gmail.com>
2023-09-18 03:18:59 -0700

    main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,7 +7,7 @@
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-int _strlen(char *s); 
+int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
@@ -17,3 +17,4 @@ void print_arry(int *a, int n);
 char *_strcpy(char *dest, char *crs);
 
 #endif
+~                                  
