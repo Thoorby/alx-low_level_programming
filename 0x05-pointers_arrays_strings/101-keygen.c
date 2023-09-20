14ea028aba353d3dc76c4833d2f7f55c391ef1cb
Thoorby <ojothrby@gmail.com>
2023-09-20 04:49:43 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -19,7 +19,8 @@ int main(void)
 
 	for (i = 0; i < 100; i++)
 	{
-		pass[i] = rand() % 78;
+
+        	pass[i] = rand() % 78;
 		sum += (pass[i] + '0');
 		putchar(pass[i] + '0');
 		if ((2772 - sum) - '0' < 78)
