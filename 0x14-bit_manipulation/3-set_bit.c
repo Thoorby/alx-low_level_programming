167632004ac27ade6eecd33462fb1b86a3b599d1
Thoorby <ojothrby@gmail.com>
2023-10-27 10:50:56 -0700

    3-set_bit.c

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,13 @@
+#include <main.h>
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+    if (index >= sizeof(unsigned long int) * CHAR_BIT)
+    {
+        return -1;
+    }
+
+    *n |= 1UL << index;
+
+    return 1;
+}
